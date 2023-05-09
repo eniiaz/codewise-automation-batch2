86830798e3030a78c06c321ef7967796f9272450
eniiazov <esenniiazov@gmail.com>
2023-05-09 11:20:22 -0500

    Locators

---
+++ src/test/java/saucedemo/Login.java
@@ -0,0 +1,36 @@
+package saucedemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class Login {
+
+    public static void main(String[] args) throws InterruptedException {
+        // https://www.saucedemo.com/
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        WebElement username = driver.findElement(By.id("user-name"));
+        username.sendKeys("helloCodewise!");
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("password!");
+
+        Thread.sleep(2000);
+
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
+        WebElement errorMessage = driver.findElement(By.tagName("h3"));
+        String message = errorMessage.getText();
+        String expectedErrorMessage = "Epic sadface: Username and password do not match any user in this service";
+        if (expectedErrorMessage.equals(message)){
+            System.out.println("Error message passed");
+        }else {
+            System.out.println("Error message failed");
+        }
+        //driver.quit();
+    }
+}
---
+++ src/test/java/saucedemo/LoginPositive.java
@@ -0,0 +1,40 @@
+package saucedemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPositive {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://saucedemo.com");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        String expectedURL = "https://www.saucedemo.com/inventory.html";
+        String actualURL = driver.getCurrentUrl();
+        if (expectedURL.equals(actualURL)){
+            System.out.println("Login successfully passed");
+        }else {
+            System.out.println("Login failed!");
+        }
+        driver.findElement(By.linkText("Sauce Labs Backpack")).click();
+        Thread.sleep(500);
+        driver.navigate().back();
+
+        driver.findElement(By.linkText("Sauce Labs Bike Light")).click();
+        Thread.sleep(500);
+        driver.navigate().back();
+
+        driver.findElement(By.linkText("Sauce Labs Bolt T-Shirt")).click();
+        Thread.sleep(500);
+        driver.navigate().back();
+
+        driver.findElement(By.linkText("Sauce Labs Fleece Jacket")).click();
+        Thread.sleep(500);
+        driver.navigate().back();
+
+    }
+}
