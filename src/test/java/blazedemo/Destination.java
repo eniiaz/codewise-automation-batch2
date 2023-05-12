06ba4b15ea42946c0e5b872ab9eaf95adf3e15ef
Esen Niiazov <eniiazov@gmail.com>
2023-05-12 11:51:43 -0500

    implicit wait

--- src/test/java/blazedemo/Destination.java
+++ src/test/java/blazedemo/Destination.java
@@ -7,12 +7,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.time.Duration;
+
 public class Destination {
 
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://blazedemo.com");
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(12));
 
         WebElement destinationDropdown = driver.findElement(By.name("toPort"));
         Select destinationCities = new Select(destinationDropdown);
